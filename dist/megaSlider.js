49c3a219cbcfb4cb1f0932a5a99c501b66f6215b
Trambitskiy Yura <tr.yura@yahoo.com>
2013-12-11 14:07:34 +0400

    added basic functions

--- dist/megaSlider.js
+++ dist/megaSlider.js
@@ -1,3 +1,36 @@
-function Hi() {
+;
+(function($) {
+    $.fn.megaSlider = function(options) {
 
-}
+        var $this = $(this);
+
+        //setting options
+        var defaults = {
+            auto: false,
+            pause: 5000,
+            duration: 500,
+            horizontalBlocks: 8,
+            verticalBlocks: 4,
+            afterSlide: function() {},
+            beforeSlide: function() {}
+        };
+
+        options = $.extend(defaults, options);
+
+        function changeSlide(effect) {
+
+        }
+
+        function calculateBlockHeightAndWidth() {
+            var width = $this.width(),
+                height = $this.height(),
+                blockWidth = width/options.horizontalBlocks;
+                blockHeight = width/options.verticalBlocks;
+                return {
+                    blockWidth: blockWidth,
+                    blockHeight: blockHeight
+                }
+        }
+
+    }
+})(jQuery);
