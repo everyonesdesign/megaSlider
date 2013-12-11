170e7a64f0eeb842856e13b976b3793e5285bb40
Trambitskiy Yura <tr.yura@yahoo.com>
2013-12-11 18:06:43 +0400

    began writing slider init and css

--- dist/megaSlider.js
+++ dist/megaSlider.js
@@ -2,48 +2,74 @@
 (function($) {
     $.fn.megaSlider = function(options) {
 
-        var $this = $(this),
-            autoTimeout;
+        //loop for multiple sliders call
+        $(this).each(function() {
 
-        //setting options
-        var defaults = {
-            auto: false,
-            pause: 5000,
-            duration: 500,
-            horizontalBlocks: 8,
-            verticalBlocks: 4,
-            pauseOnHover: true,
-            beforeSlide: function() {},
-            afterSlide: function() {}
-        };
+            //setting options
+            var defaults = {
+                auto: false,
+                pause: 5000,
+                duration: 500,
+                horizontalBlocks: 8,
+                verticalBlocks: 4,
+                pauseOnHover: true,
+                startSlide: 0,
+                beforeSlide: function() {},
+                afterSlide: function() {}
+            };
 
-        options = $.extend(defaults, options);
+            options = $.extend(defaults, options);
 
-        //slider initialization
-        function initSlider() {
+            var $this = $(this),
+                $images = $this.find("img"),
+                $slides = $images.wrap("<div class='megaSlider-slide'>"),
+                currentSlide,
+                nextSlide,
+                autoTimeout;
 
-        }
-
-        //main change slide function
-        function changeSlide(effect) {
-            options.beforeSlide();
-
-
-
-            options.afterSlide();
-        }
-
-        //calculate width and height of blocks before transition
-        function calculateBlockHeightAndWidth() {
-            var width = $this.width(),
-                height = $this.height(),
-                blockWidth = width/options.horizontalBlocks;
-                blockHeight = width/options.verticalBlocks;
-            return {
-                blockWidth: blockWidth,
-                blockHeight: blockHeight
+            //slider initialization
+            initSlider();
+            function initSlider() {
+                var heightToSet = calculateMinHeight();
+                $this.addClass("megaSlider").height(heightToSet);
+                currentSlide = options.startSlide;
+                nextSlide = options.startSlide + 1;
             }
-        }
+
+            //main change slide function
+            function changeSlide(effect) {
+                options.beforeSlide();
+
+
+
+                options.afterSlide();
+            }
+
+            //calculate width and height of blocks before transition
+            function calculateBlockHeightAndWidth() {
+                var width = $this.width(),
+                    height = $this.height(),
+                    blockWidth = width/options.horizontalBlocks;
+                blockHeight = width/options.verticalBlocks;
+                return {
+                    blockWidth: blockWidth,
+                    blockHeight: blockHeight
+                }
+            }
+
+            //function to calculate minimal height among images
+            function calculateMinHeight() {
+                var minHeight = Infinity;
+                $images.each(function() {
+                    var thisHeight = $(this).height();
+                    if (thisHeight < minHeight) {
+                        minHeight = thisHeight;
+                    }
+                });
+                return minHeight;
+            }
+
+        });
 
         return this;
     }
